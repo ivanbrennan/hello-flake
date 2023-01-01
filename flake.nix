9a1d79138edf28638b6315d1ad476dd259a8ad4f
ivanbrennan <ivan.brennan@gmail.com>
2023-01-01 13:58:16 -0500

    flake.nix
    Following along with this tutorial:
    https://www.tweag.io/blog/2020-05-25-flakes/
    
    The tutorial is a few years old, and I noticed that the flake.nix
    generated by running `nix flake init` differs somewhat from what the
    tutorial described.
    
    Some relevant commands:
    
      nix --extra-experimental-features 'flakes' flake init
      nix --extra-experimental-features 'flakes' build
      nix --extra-experimental-features 'flakes' shell --command hello
      nix --extra-experimental-features 'flakes' flake metadata
    
      echo $SHLVL
      nix --extra-experimental-features 'flakes' develop
      echo $SHLVL
      eval "$buildPhase"
      ./hello

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A flake for building Hello Flake";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.03;
+
+  outputs = { self, nixpkgs }: {
+
+    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    defaultPackage.x86_64-linux =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "hello";
+        src = self;
+        buildPhase = "gcc -o hello ./hello.c";
+        installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+      };
+  };
+}
